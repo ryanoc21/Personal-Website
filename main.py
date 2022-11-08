d2c93bc1c0eb6cc56b71ca15a784fc1242d6bbaa
ryanoconnor <ryan19.roc@gmail.com>
2022-11-08 10:25:41 +0000

    Print a message, render the app titles into columns.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 
 st.set_page_config(layout='wide')
 
@@ -16,3 +17,19 @@ with col2:
     while learning Python. More projects will be added to it over time as my skills progress. 
     """
     st.info(content)
+
+message = """
+Below you can find some of my projects. Feel free to contact me !
+"""
+st.write(message)
+
+col3, col4 = st.columns(2)
+
+df = pd.read_csv("data.csv", sep=";")
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row['title'])
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row['title'])
