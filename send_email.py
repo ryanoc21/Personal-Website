b89f5567bba79bb6de69e39a0b90721e579ed90c
ryanoconnor <ryan19.roc@gmail.com>
2022-11-14 22:18:19 +0000

    Add new image files and make changes to list range in home file

--- send_email.py
+++ send_email.py
@@ -4,11 +4,11 @@ import os
 
 
 def send_email(message):
-    host = "#"
-    port =
-    username = "#"
+    host = "smtp.gmail.com"
+    port = 465
+    username = "ryanconnor851@gmail.com"
     password = os.environ.get("PASSWORD")
-    receiver = "#"
+    receiver = "ryanconnor851@gmail.com"
     context = ssl.create_default_context()
     with smtplib.SMTP_SSL(host, port, context=context) as server:
         server.login(username, password)
