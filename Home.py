69028c86fa06c5ffe0982c5fb3532bae28308e23
ryanoconnor <ryan19.roc@gmail.com>
2022-11-16 08:40:05 +0000

    Add statistics project and corresponding image

--- Home.py
+++ Home.py
@@ -28,14 +28,14 @@ col3, empty_col, col4 = st.columns([1.5,0.5,1.5])
 
 df = pd.read_csv("data.csv", sep=";")
 with col3:
-    for index, row in df[:9].iterrows():
+    for index, row in df[:6].iterrows():
         st.header(row['title'])
         st.write(row['description'])
         st.image("images/" + row["image"])
         st.write(f"[Source Code]({row['url']})")
 
 with col4:
-    for index, row in df[9:].iterrows():
+    for index, row in df[6:].iterrows():
         st.header(row['title'])
         st.write(row['description'])
         st.image("images/" + row["image"])
